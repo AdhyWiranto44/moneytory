bb1912dcd1c212edbad656df51a26ff97ff6eb1b
adhywiranto44 <adhywiranto68@gmail.com>
2022-01-09 19:13:19 +0700

    feat: company logo in sidebar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,9 +19,12 @@ class DashboardController extends Controller
             return redirect('/login');
         }
 
+        $company = CompanyProfile::first();
         $username = User::firstWhere('username', $request->session()->get('username'))->username;
         $data = [
-            'username' => $username
+            'username' => $username,
+            'companyName' => $company->name,
+            'companyLogo' => $company->image,
         ];
         return view('dashboard', $data);
     }
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,8 +1,12 @@
 <div id="sidebar" class="sidebar bg-light-salmon position-fixed h-100 overflow-auto border-end border-light border-2" style="z-index: 1;">
     <div id="company-image" class="d-flex justify-content-center my-4 overflow-hidden position-relative">
-        <img class="rounded-circle" src="/img/default.jpg" alt="default" width="64" height="64">
+        @if ($companyLogo)
+            <img class="rounded-circle" src="{{ asset('storage/img/' . $companyLogo) }}" alt="Company Logo" width="64" height="64">
+            @else
+            <img class="rounded-circle" src="/img/default.jpg" alt="default" width="64" height="64">
+        @endif
     </div>
-    <h5 class="company-name fw-bold text-center">Kotlin Tbk.</h5>
+    <h5 class="company-name fw-bold text-center">{{ $companyName }}</h5>
     <ul class="sidebar-menu-container">
         <li class="sidebar-menu-section mt-3">
             <ul>
