1b0cfafbde9f60735dd7eacaf41ef54d435d939a
adhywiranto44 <adhywiranto68@gmail.com>
2022-01-09 14:26:27 +0700

    fix: redirect ke halaman login jika sudah ada data perusahaan

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CompanyProfile;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
+        $companyProfile = CompanyProfile::first();
+        if ($companyProfile != null) {
+            return redirect('/login');
+        }
         return view('welcome');
     }
 }
