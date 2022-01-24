a1de6d3fe1a4565f64f60ebf10ca025a89be444d
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-01-24 09:01:56 +0700

    feat: gambar sekarang di form ubah bahan mentah

--- resources/views/raw_ingredient_edit.blade.php
+++ resources/views/raw_ingredient_edit.blade.php
@@ -72,6 +72,14 @@
                     <div class="mb-3">
                         <label for="image" class="form-label small mb-1 text-capitalize">gambar</label>
                         <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image">
+                        <div class="previous-image mt-3">
+                            <p class="mb-0">Gambar Sekarang</p>
+                            @if ($rawIngredient->image)
+                                <img class="rounded-circle me-2 d-none d-md-block" src="{{ asset('storage/img/' . $rawIngredient->image) }}" alt="barang jadi" width="36" height="36">
+                            @else
+                                <img class="rounded-circle me-2 d-none d-md-block" src="/img/default.jpg" alt="default" width="36" height="36">
+                            @endif
+                        </div>
                         @error('image')
                             <div class="invalid-feedback">
                                 {{ $message }}
