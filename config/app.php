e98ac944990e53bf727bff2eec37071820d0649e
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-02-02 06:52:19 +0700

    fix: change timezone

--- config/app.php
+++ config/app.php
@@ -67,7 +67,8 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Jakarta'),
+    // 'timezone' => env('APP_TIMEZONE', 'UTC'),
 
     /*
     |--------------------------------------------------------------------------
